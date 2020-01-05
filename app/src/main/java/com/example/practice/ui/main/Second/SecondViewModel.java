d910e1f1bd976c61ae345e1eccc3988670c54faa
seul <ths06123@gmail.com>
2020-01-05 22:58:37 +0900

    mm

--- app/src/main/java/com/example/practice/ui/main/Second/SecondViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.practice.ui.main.Second;
-
-import androidx.lifecycle.ViewModel;
-
-public class SecondViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/practice/ui/main/Third/ThirdViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.practice.ui.main.Third;
-
-import androidx.lifecycle.ViewModel;
-
-public class ThirdViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
