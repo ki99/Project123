267afd663a7ee70b5dcf1d8b86a24166d8cdb371
root <ths06123@gmail.com>
2020-01-05 07:51:44 +0000

    commit

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var mongoDB = 'mongodb://127.0.0.1:27017/data'
 mongoose.connect(mongoDB, {useNewUrlParser : true});
 
 // DEFINE MODEL
-var Contact = require('./models/book');
+var Contact = require('./models/cont');
 
 // [CONFIGURE APP TO USE bodyParser]
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,39 +39,3 @@ var router = require('./routes')(app, Contact);
 var server = app.listen(80, function(){
  console.log("Express server has started on port " + 80)
 });
-
-
-
-
-/* var kittySchema = new mongoose.Schema({
-    name: String
-  });
-
-  var Kitten = mongoose.model('Kitten', kittySchema);
-
-  var silence = new Kitten({ name: 'Silence' });
-console.log(silence.name); // 'Silence'
-
-kittySchema.methods.speak = function () {
-    var greeting = this.name
-      ? "Meow name is " + this.name
-      : "I don't have a name";
-    console.log(greeting);
-  }
-  
-  var Kitten = mongoose.model('Kitten', kittySchema);
-
-  var fluffy = new Kitten({ name: 'fluffy' });
-  fluffy.speak(); // "Meow name is fluffy"
-
-  fluffy.save(function (err, fluffy) {
-    if (err) return console.error(err);
-    fluffy.speak();
-  });
-
-  Kitten.find(function (err, kittens) {
-    if (err) return console.error(err);
-    console.log(kittens);
-  })
-
-  Kitten.find({ name: /^fluff/ }, callback); */
