70d877d25d656e6b477caac0933f8226dbafb051
seul <ths06123@gmail.com>
2020-01-07 23:37:36 +0900

    타슈어때

--- Project123-googleMap/app/src/main/java/com/example/myapp/MainActivity.java
+++ Project123-googleMap/app/src/main/java/com/example/myapp/MainActivity.java
@@ -865,7 +865,7 @@ public class MainActivity extends AppCompatActivity
     public void setDefaultLocation() {
 
         //디폴트 위치, Seoul
-        LatLng DEFAULT_LOCATION = new LatLng(37.56, 126.97);
+        LatLng DEFAULT_LOCATION = new LatLng(36.374160, 127.365790);
         String markerTitle = "위치정보 가져올 수 없음";
         String markerSnippet = "위치 퍼미션과 GPS 활성 요부 확인하세요";
 
